dc40ac3d91ca85673d1c423d790b328747180a92
Nicholas Manning <nicholas.manning@gmail.com>
2020-08-28 19:47:13 -0400

    wip

---
+++ lib/pages/home.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/material.dart';
+
+class Home extends StatefulWidget {
+  Home({Key key}) : super(key: key);
+
+  @override
+  _HomeState createState() => _HomeState();
+}
+
+class _HomeState extends State<Home> {
+  _HomeState();
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("OAuth Example"),
+      ),
+    )
+  }
+}
